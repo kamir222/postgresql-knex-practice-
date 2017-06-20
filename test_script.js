34e69bad69d9248afda9c948c56650e50762b7f5
kamir222 <katia.amir@gmail.com>
2017-06-20 16:27:04 +0000

    added a new file that is sql

--- test_script.js
+++ test_script.js
@@ -1,9 +1,8 @@
-const pg = require("pg"); // requiring node-postgres
-const settings = require("./settings"); // requiring settings.json
+const pg = require("pg"); //postgres package
+const settings = require("./settings"); // settings.json
 
-//create a config to configure pooling behaviour and client options
-//take this information from the /settings.json file
-const client = new new pg.Client({
+// create a config to configure both pooling behavior and client options
+const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
   database : settings.database,
@@ -16,9 +15,9 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::init AS number"), ["1"], (err, result) => {
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
     if (err) {
-      return console.error("error running query", err)
+      return console.error("error running query", err);
     }
     console.log(result.rows[0].number); //output: 1
     client.end();
