c0d84b0a524e8dc466f4951001bd6f451ac7e490
kamir222 <katia.amir@gmail.com>
2017-06-20 23:38:04 +0000

    added an auto increment id to the table in milestones

--- migrations/20170620230546_create_milestones.js
+++ migrations/20170620230546_create_milestones.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table){
+      table.increments();
       table.string('description');
       table.date('date_achieved ');
     })
@@ -10,6 +11,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-      knex.schema.dropTable('milestones');
+      knex.schema.dropTable('milestones')
   ])
 };
