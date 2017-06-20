6c20a1d6356c65871267df17ed6c88a91ae2b2d4
kamir222 <katia.amir@gmail.com>
2017-06-20 20:57:20 +0000

    modified script to be able to query the famous_people.sql

--- famous_people.sql
+++ famous_people.sql
@@ -11,3 +11,8 @@ INSERT INTO famous_people (first_name, last_name, birthdate)
   VALUES ('Mahatma', 'Gandhi', '1869-10-02');
 INSERT INTO famous_people (first_name, last_name, birthdate)
   VALUES ('Paul', 'Rudd', '1969-04-06');
+
+
+SELECT * FROM famous_people; -- should give us back all 3
+SELECT * FROM famous_people WHERE birthdate >= '1920-01-01';
+SELECT * FROM famous_people WHERE birthdate < '1920-01-01';
--- test_script.js
+++ test_script.js
@@ -1,25 +1,50 @@
+"strict";
 const pg = require("pg"); //postgres package
 const settings = require("./settings"); // settings.json
+const keyword = process.argv[2];
+// how do I export famous_people.sql into this file to run queries?
+
+const input = `
+  SELECT * FROM famous_people
+  WHERE first_name = $1::text`
+  //sql value that selects all entity instances
+  //from a table where first_name is a text
 
 // create a config to configure both pooling behavior and client options
+//location of database --> host and port
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
 
-client.connect((err) => {
+
+
+client.connect((err) => { //establish a connection with the database
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+
+
+  client.query(input, [keyword], (err, result) => { //make a query on that database
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log("Searching...");
+    output(result.rows);
     client.end();
   });
 });
+
+const output = (rows) => {
+  console.log(`Found ${rows.length} person(s) by the name of ${keyword}.`);
+  console.log(`- ${rows[0].id}: ${rows[0].first_name}`);
+
+  // THIS IS ANOTHER WAY OF DOING LINE 44
+  // for (let row of rows) {
+  //   console.log(`- ${row.id}: ${row.first_name}`);
+  // }
+}
